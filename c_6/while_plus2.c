eb8a0e40b5db7eed4d8c129e3e6fbbc4f3b623a2
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-04-06 14:31:50 +0900

    Update while_plus2.c

--- c_6/while_plus2.c
+++ c_6/while_plus2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(){
-  int n, i=0, sum=0;
+  int n, i=0, sum=0;    //전역변수 => main이 유지되는 동안 메모리에 존재
 
   while(i<5){
-    printf("값을 입력하시오: ");
+    printf("값을 입력하시오: ");    //지역변수 => while 안에서만 선언할 경우 while 끝나면 메모리에서 지워짐
     scanf("%d",&n);
 
     sum += n;
