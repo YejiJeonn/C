97e63bc3bc2d8ffb9e51331ee84db1a52b9a5830
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-04-06 16:00:36 +0900

    Create testcase_for.c

---
+++ c_6/testcase_for.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+  int i, n, a=0, b=0;
+
+  printf("반복할 횟수를 입력하시오: ");
+  scanf("%d",&n);
+
+  for(i=0; i<n; i++){
+    scanf ("%d %d", &a, &b);
+    printf("%d\n", a+b);
+  }
+
+  return 0;
+}
