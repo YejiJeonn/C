721426574337029324733dbfae725d8659450aaa
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-04-06 16:04:42 +0900

    Create testcase_dowhile.c

---
+++ c_6/testcase_dowhile.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+  int i=0, n, a=0, b=0;
+
+  printf("반복할 횟수를 입력하시오: ");
+  scanf("%d",&n);
+
+  do{
+    scanf("%d %d", &a, &b);
+    printf("%d\n",a+b);
+    i++;
+  }while(i != 5);
+
+  return 0;
+}
