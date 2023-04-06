b5c163680f041818aca88af76a9e9dbd95b62315
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-04-06 14:10:21 +0900

    Create while_plus.c

---
+++ c_6/while_plus.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+  int n, i=1, sum=0;
+
+  printf("정수를 입력하시오 : ");
+  scanf("%d",&n);
+
+  while(i<=n){
+    sum +=i;
+    i++;
+  }
+
+  printf("1부터 3까지의 합은 %d 입니다.",sum);
+  
+  return 0;
+}
