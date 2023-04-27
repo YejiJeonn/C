ea124d3d2d4d93007d8348d78290ef7f500a9700
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-04-27 14:27:20 +0900

    Create 2-4.c

---
+++ c_middletest/2-4.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void){
+    int won;
+    double rate, usd;
+
+    printf("환율을 입력하시오: ");
+    scanf("%lf", &rate);
+
+    printf("원화 금액을 입력하시오: ");
+    scanf("%d", &won);
+
+    usd = won/rate;
+    printf("원화 %d원은 %lf달러 입니다.\n", won, usd);
+
+    return 0;
+}
