53d80934b04fa07a6d41284c575640ec389db96c
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-04-27 15:27:05 +0900

    Create 3-4.c

---
+++ c_middletest/3-4.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(){
+    char code1 = 'A';
+    char code2 = 65;
+
+    printf("code1 = %c\n", code1);
+    printf("code2 = %c\n", code2);
+}
