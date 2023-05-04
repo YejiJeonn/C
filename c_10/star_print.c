898161b58ea4de809081bc03558dcc02522d7a44
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-05-04 14:36:17 +0900

    Create star_print.c

---
+++ c_10/star_print.c
@@ -0,0 +1,25 @@
+// 공백 이용해서 별 찍기
+
+#include <stdio.h>
+
+int main(){
+  int i, j, n;
+
+  printf("숫자를 입력하시오: ");
+  scanf("%d", &n);
+
+  for(i=1; i<=n; i++){
+
+    for(j=1; j<=n-i; j++){
+      printf(" ");
+    }
+    for(j=1; j<=i; j++){
+        printf("*");
+    }
+
+    printf("\n");
+  }
+
+
+  return 0;
+}
