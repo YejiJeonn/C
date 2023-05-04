0dbadc25474e581444431ca4b13bcaedd8b73d71
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-05-04 15:34:59 +0900

    Update factorial.c

--- c_10/factorial.c
+++ c_10/factorial.c
@@ -2,19 +2,26 @@
 
 #include <stdio.h>
 
+// 팩토리얼 함수
+int factorial(int n){
+  int sum =1, i;
+
+  for(i=1; i<=n; i++){
+    sum *= i;
+  }
+
+  return sum;
+}
+
 int main(){
-  int x, i, sum=1;
+  int x, i;
 
   printf("알고 싶은 팩토리얼의 값은? ");
   scanf("%d", &x);
 
-  for(i=1; i<=x; i++){
-    sum *= i;
-  }
-
-  printf("%d!의 값은 %d 입니다.\n", x, sum);
+  printf("%d!의 값은 %d 입니다.\n", x, factorial(x));
 
   return 0;
 }
 
-// sum을 0으로 초기화할 경우 팩토리얼값이 0이 나오므로 초기화할 때 1로 할 것
+// for문에서 i를 0으로 초기화하거나 sum을 0으로 초기화할 경우 팩토리얼값이 0이 나오므로 초기화할 때 1로 할 것
