3e5c23526bc79efe9b97b22ceb12a3dc157519ea
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-05-12 15:52:55 +0900

    Create p76.c

---
+++ Task/p76.c
@@ -0,0 +1,26 @@
+// 기타 함수 출력
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  
+  system("dir");
+  printf("\n");
+
+  system("copy");
+  printf("\n");
+
+  system("type");
+  printf("\n");
+
+  system("cls");
+  printf("\n");
+
+  system("del");
+  printf("\n");
+
+  system("mkdir");
+  printf("\n");
+
+  return 0;
+}
