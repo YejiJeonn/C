96379aa2e5b4a5404247116c7677eebbeaf3489b
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-05-30 22:20:43 +0900

    Update 7_p24.c

--- c_finals/7_p24.c
+++ c_finals/7_p24.c
@@ -2,15 +2,15 @@
 
 #include <stdio.h>
 
-
+// max 함수
 int max(int x, int y){
-  if(x>y){
+  if(x>y){            // x가 y보다 클 경우
     return x;
   }
-  else if(x<y){
+  else if(x<y){       // y가 x보다 클 경우
     return y;
   }
-  else{
+  else{               // x와 y가 같을 경우
     return 0;
   }
 }
@@ -21,11 +21,11 @@ int main(){
 
   printf("2개의 정수를 입력하세요 : ");
   scanf("%d %d", &x, &y);
-
-  if(max(x, y) == 0){
+  
+  if(max(x, y) == 0){                     // max 함수의 리턴 값이 0일경우 == x와 y에 같은 값을 입력한 경우
     printf("수를 잘못 입력하셨습니다.\n");
   }
-  else{
+  else{                                   // 정상적으로 처리됐을 경우
     printf("더 큰 값은 %d 입니다.\n", max(x, y));
   }
 
