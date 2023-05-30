22c7421367499acaad5b7f45e9d71cd811dfef77
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-05-30 22:52:18 +0900

    Create TB_p292.c

---
+++ c_finals/TB_p292.c
@@ -0,0 +1,21 @@
+// 피타고라스 정리
+
+# include <stdio.h>
+
+int main(){
+  int a, b, c;
+
+  for(a=1; a<=100; a++){
+    for(b=1; b<=100; b++){
+      for(c=1; c<=100; c++){
+        // a<b 조건을 삽입해서 중복되는 항목을 제거함
+        if((a<b)&&((a*a+b*b) == c*c)){
+          printf("%d %d %d\n", a, b, c);
+        }
+      }
+    }
+  }
+
+
+  return 0;
+}
